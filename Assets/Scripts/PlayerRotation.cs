b853e5b8302427062f34252f7ffa91c68f95554e
vincentbernardprogit <vincent.bernard.corp@gmail.com>
2026-08-15 04:59:28 +0700

    Create project

---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public Transform characterBody;
+    public float rotationSensitivity = 750f;
+    float xRotation = 0f;
+    [SerializeField] private float clampDegreeUp = 70f;
+    private float clampDegreeDown = 0f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, clampDegreeDown, clampDegreeUp);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        characterBody.Rotate(Vector3.up * mouseX);
+    }
+}
